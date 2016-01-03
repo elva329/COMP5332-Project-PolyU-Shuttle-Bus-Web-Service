bcfcb70f2f7b4348f8ee72f26a279db87d9425f9
Jia Shen <elva329@126.com>
2016-01-03 18:37:51 +0800

    Create controller_timetable.js

---
+++ public/controllers/controller_timetable.js
@@ -0,0 +1,64 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('appCtrl',['$scope','$http',
+	function($scope,$http){
+		console.log("hello world");
+
+		//"get" request to the server
+		
+		
+		$http.get('/stops').success(
+			function(response){
+				console.log("i get the data i requested");
+				$scope.route=response; 
+				//get the response and give the value to "$scope.route"
+			});
+				
+		var refresh=function(){
+			$http.get('/stops').success(function(response){
+				console.log("i get the data i requested");
+				$scope.stop=response;
+				$scope.item="";
+			});
+		};
+
+		$scope.remove=function(id){
+			console.log(id);
+			$http.delete('/delete/'+id).success(
+					function(response){
+						refresh();
+					});
+		};
+
+		$scope.add=function(){
+			console.log($scope.item);
+			$http.post('/add',$scope.item).success(
+			function(response){
+				console.log(response);
+				refresh();
+				$scope.item="";
+			});
+		};
+
+		$scope.edit=function(id){
+			console.log(id);
+			$http.get('/edit/'+id).success(function(response){
+				$scope.item=response;
+			});
+		};
+
+		$scope.update=function(){
+			console.log($scope.item._id);
+			$http.put('/update/'+$scope.item._id,$scope.item).success(function(response){
+				refresh();
+			});
+		};
+
+		$scope.openTab = function() {
+   		 $scope.url = 'timetable_template.html';
+		}
+
+
+	}]);
+
+		
